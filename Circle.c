ec2ab436c98082991dcea704dca036dea3d2149e
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2024-01-26 17:56:36 -0500

    Update Circle.c

---
+++ Circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    const double PI = 3.14159;
+    double radius;
+    double circumference;
+
+    printf("\nEnter radius of a circle: %f\n", radius);
+    scanf("%1f", &radius);
+
+    
+    return 0;
+}
